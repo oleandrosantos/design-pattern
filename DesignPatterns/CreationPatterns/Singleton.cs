656d658f712ea3ed7b354abe9d8a9a650dc31573
Leandro Santos <leandro.santos@consuti.com.br>
2022-06-16 23:38:40 -0300

    initial commit

---
+++ DesignPatterns/CreationPatterns/Singleton.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DesignPatterns.CreationPatterns
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        
+        //bloqueio responsavel por sincronizar as threads
+        private static readonly object _instanceLock = new object();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null) 
+            {
+                lock (_instanceLock)
+                {
+                    if(_instance == null)
+                        _instance = new Singleton();
+                }
+            }
+            
+            return _instance;
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.CreationPatterns;
+
+Console.WriteLine("** Singleton ** ");
+Singleton s1 = Singleton.GetInstance();
+Singleton s2 = Singleton.GetInstance();
+
+string ResultIsEquals = s1 == s2 ? "São iguais" : "São diferentes";
+Console.WriteLine(ResultIsEquals);
