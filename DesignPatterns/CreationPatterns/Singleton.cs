3f07309af626c368173cd1234d8cb4be3086a7c1
Leandro Santos <leandro.santos@consuti.com.br>
2022-06-16 23:50:43 -0300

    feat: implementando trava de thread

--- DesignPatterns/CreationPatterns/Singleton.cs
+++ DesignPatterns/CreationPatterns/Singleton.cs
@@ -4,23 +4,26 @@ namespace DesignPatterns.CreationPatterns
     public class Singleton
     {
         private static Singleton _instance;
-        
+
         //bloqueio responsavel por sincronizar as threads
         private static readonly object _instanceLock = new object();
-
+        public string InformacaoImutavel { get; set; }
         private Singleton() { }
 
-        public static Singleton GetInstance()
+        public static Singleton GetInstance(string dadoImutavel)
         {
-            if (_instance == null) 
+            if (_instance == null)
             {
                 lock (_instanceLock)
                 {
-                    if(_instance == null)
+                    if (_instance == null)
+                    {
                         _instance = new Singleton();
+                        _instance.InformacaoImutavel = dadoImutavel;
+                    }
                 }
             }
-            
+
             return _instance;
         }
     }
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -2,8 +2,8 @@
 using DesignPatterns.CreationPatterns;
 
 Console.WriteLine("** Singleton ** ");
-Singleton s1 = Singleton.GetInstance();
-Singleton s2 = Singleton.GetInstance();
+Singleton s1 = Singleton.GetInstance("Dado 1");
+Singleton s2 = Singleton.GetInstance("Dado 1");
 
 string ResultIsEquals = s1 == s2 ? "São iguais" : "São diferentes";
 Console.WriteLine(ResultIsEquals);
